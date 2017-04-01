88b971356a8faba82edb96a7ffa77400754a6f65
niciyan <nishiyamasu@gmail.com>
2017-04-01 19:04:10 +0900

    changeed view

--- app/main/views.py
+++ app/main/views.py
@@ -6,14 +6,9 @@ from ..models import Message
 from .forms import NameForm
 from datetime import datetime
 
-@main.route('/')
+@main.route('/', methods=['GET', 'POST'])
 @login_required
 def index():
-    return render_template('index.html', messages=Message().query.order_by(Message.date.desc()).limit(6).all())
-
-@main.route('/new', methods=['GET', 'POST'])
-@login_required
-def new():
     form = NameForm() 
     # if request.method == 'POST':
     if form.validate_on_submit():
@@ -24,8 +19,29 @@ def new():
         db.session.commit()
         flash('新しいメッセージを追加しました!!')
         current_app.logger.debug('new data inserted.')
-        return redirect(url_for('main.index'))
+        # return redirect(url_for('main.index'))
         print('passed')
-    else:
-        return render_template('new.html', form=form)
+    # else:
+    #     return render_template('new.html', form=form)
+    return render_template('index.html',
+            messages=Message().query.order_by(Message.date.desc()).limit(6).all(),
+            form=form)
+
+# @main.route('/new', methods=['GET', 'POST'])
+# @login_required
+# def new():
+#     form = NameForm() 
+#     # if request.method == 'POST':
+#     if form.validate_on_submit():
+#         # form.text.data
+#         now = datetime.now()
+#         me = Message(message=form.text.data, date=now)
+#         db.session.add(me)
+#         db.session.commit()
+#         flash('新しいメッセージを追加しました!!')
+#         current_app.logger.debug('new data inserted.')
+#         return redirect(url_for('main.index'))
+#         print('passed')
+#     else:
+#         return render_template('new.html', form=form)
 
